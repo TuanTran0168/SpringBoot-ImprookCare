eeea51c26334ca131f59dbcd8a490a3485fd5908
TuanTran <tamthy12345@gmail.com>
2023-10-04 14:47:24 +0700

    add api vnpay

---
+++ IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/controllers/ApiVNPAYController.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tuantran.IMPROOK_CARE.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import com.google.gson.Gson;
+//import com.google.gson.JsonObject;
+import com.tuantran.IMPROOK_CARE.configs.vnpay.VNPAYConfig;
+import com.tuantran.IMPROOK_CARE.dto.VNPAYDTO;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Administrator
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiVNPAYController {
+
+//    @GetMapping("/public/create-payment/")
+//    public ResponseEntity<?> createPayment() throws UnsupportedEncodingException {
+//        String vnp_Version = "2.1.0";
+//        String vnp_Command = "pay";
+//        long amount = 5000 * 100;
+////        String orderType = "other";
+////        long amount = Integer.parseInt(req.getParameter("amount")) * 100;
+////        String bankCode = req.getParameter("bankCode");
+//
+//        String vnp_TxnRef = VNPAYConfig.getRandomNumber(8);
+////        String vnp_IpAddr = VNPAYConfig.getIpAddress(req);
+//
+//        String vnp_TmnCode = VNPAYConfig.vnp_TmnCode;
+//
+//        Map<String, String> vnp_Params = new HashMap<>();
+//        vnp_Params.put("vnp_Version", vnp_Version);
+//        vnp_Params.put("vnp_Command", vnp_Command);
+//        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+//        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+//        vnp_Params.put("vnp_CurrCode", "VND");
+//        vnp_Params.put("vnp_BankCode", "NCB"); // Tài khoản test cho xài có cái này
+//        vnp_Params.put("vnp_Locale", "vn");
+//        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+//        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang cua Tuan Tran:" + vnp_TxnRef);
+//
+////        if (bankCode != null && !bankCode.isEmpty()) {
+////            vnp_Params.put("vnp_BankCode", bankCode);
+////        }
+////        vnp_Params.put("vnp_OrderType", orderType);
+////        String locate = req.getParameter("language");
+////        if (locate != null && !locate.isEmpty()) {
+////            vnp_Params.put("vnp_Locale", locate);
+////        } else {
+////            vnp_Params.put("vnp_Locale", "vn");
+////        }
+//        vnp_Params.put("vnp_ReturnUrl", VNPAYConfig.vnp_ReturnUrl);
+//        vnp_Params.put("vnp_IpAddr", "192.168.50.46");
+//        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+//        String vnp_CreateDate = formatter.format(cld.getTime());
+//        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+//
+//        cld.add(Calendar.MINUTE, 15);
+//        String vnp_ExpireDate = formatter.format(cld.getTime());
+//        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+//
+//        List fieldNames = new ArrayList(vnp_Params.keySet());
+//        Collections.sort(fieldNames);
+//        StringBuilder hashData = new StringBuilder();
+//        StringBuilder query = new StringBuilder();
+//        Iterator itr = fieldNames.iterator();
+//        while (itr.hasNext()) {
+//            String fieldName = (String) itr.next();
+//            String fieldValue = (String) vnp_Params.get(fieldName);
+//            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+//                //Build hash data
+//                hashData.append(fieldName);
+//                hashData.append('=');
+//                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+//                //Build query
+//                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+//                query.append('=');
+//                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+//                if (itr.hasNext()) {
+//                    query.append('&');
+//                    hashData.append('&');
+//                }
+//            }
+//        }
+//        String queryUrl = query.toString();
+//        String vnp_SecureHash = VNPAYConfig.hmacSHA512(VNPAYConfig.secretKey, hashData.toString());
+//        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+//        String paymentUrl = VNPAYConfig.vnp_PayUrl + "?" + queryUrl;
+//        VNPAYDTO vnpayDTO = new VNPAYDTO();
+//
+//        vnpayDTO.setStatus("OK");
+//        vnpayDTO.setMessage("THANH TOÁN RỒI NÈ MÁ");
+//        vnpayDTO.setURL(paymentUrl);
+//
+//        return new ResponseEntity<>(vnpayDTO, HttpStatus.OK);
+////        com.google.gson.JsonObject job = new JsonObject();
+////        job.addProperty("code", "00");
+////        job.addProperty("message", "success");
+////        job.addProperty("data", paymentUrl);
+////        Gson gson = new Gson();
+////        resp.getWriter().write(gson.toJson(job));
+//    }
+
+    @GetMapping("/public/pay/")
+    public String getPay() throws UnsupportedEncodingException {
+
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String orderType = "other";
+        long amount = 10000 * 100;
+        String bankCode = "NCB";
+
+        String vnp_TxnRef = VNPAYConfig.getRandomNumber(8);
+        String vnp_IpAddr = "192.168.28.8";
+
+        String vnp_TmnCode = VNPAYConfig.vnp_TmnCode;
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+
+        vnp_Params.put("vnp_BankCode", bankCode);
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan dum Tuan Tran cai:" + vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+
+        vnp_Params.put("vnp_Locale", "vn");
+        vnp_Params.put("vnp_ReturnUrl", VNPAYConfig.vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VNPAYConfig.hmacSHA512(VNPAYConfig.secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = VNPAYConfig.vnp_PayUrl + "?" + queryUrl;
+
+        return paymentUrl;
+    }
+}
---
+++ IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/dto/VNPAYDTO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tuantran.IMPROOK_CARE.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Administrator
+ */
+
+@Getter
+@Setter
+public class VNPAYDTO {
+    private String status;
+    private String message;
+    private String URL;
+}
