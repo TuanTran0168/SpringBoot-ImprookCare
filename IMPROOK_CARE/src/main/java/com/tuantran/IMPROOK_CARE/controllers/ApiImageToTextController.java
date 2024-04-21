44f72240e26d38d090e47431ba9819c28926e777
Trần Đăng Tuấn <82080825+TuanTran0168@users.noreply.github.com>
2024-04-21 17:57:53 +0700

    [ADD][IMPROOK0011] Config Clarifai

---
+++ IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/controllers/ApiImageToTextController.java
@@ -0,0 +1,10 @@
+// package com.tuantran.IMPROOK_CARE.controllers;
+
+// import com.clarifai.grpc.api.*;
+// import com.clarifai.channel.ClarifaiChannel;
+// import com.clarifai.credentials.ClarifaiCallCredentials;
+// import com.clarifai.grpc.api.status.StatusCode;
+
+// public class ApiImageToTextController {
+
+// }
