7fb7b548e6f49e88b8bc4f825d2206e1ef504ebf
Trần Đăng Tuấn <82080825+TuanTran0168@users.noreply.github.com>
2024-04-22 16:15:00 +0700

    [UPDATE] remove all unused imports

--- IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/service/ChatgptConsultService.java
+++ IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/service/ChatgptConsultService.java
@@ -4,21 +4,14 @@
  */
 package com.tuantran.IMPROOK_CARE.service;
 
-import com.tuantran.IMPROOK_CARE.dto.AddCommentDTO;
-import com.tuantran.IMPROOK_CARE.dto.UpdateCommentDTO;
 import com.tuantran.IMPROOK_CARE.models.ChatgptConsult;
 import com.tuantran.IMPROOK_CARE.models.ChatgptQuestion;
-import com.tuantran.IMPROOK_CARE.models.Comment;
 import com.tuantran.IMPROOK_CARE.models.User;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
-import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
