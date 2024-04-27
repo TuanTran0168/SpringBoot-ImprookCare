1435032ae660fb3e0317baebd1d9124ca1fe38d1
Trần Đăng Tuấn <82080825+TuanTran0168@users.noreply.github.com>
2024-04-27 22:47:18 +0700

    [ADD][IMPROOK0022] add new API generate PDF

--- IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/controllers/ApiPDFController.java
+++ IMPROOK_CARE/src/main/java/com/tuantran/IMPROOK_CARE/controllers/ApiPDFController.java
@@ -3,6 +3,9 @@ package com.tuantran.IMPROOK_CARE.controllers;
 import java.io.FileNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,15 +45,45 @@ public class ApiPDFController {
         }
     }
 
+    // @GetMapping("/public/generate-pdf/")
+    // public void generatePDF() throws FileNotFoundException {
+    // String path = "test.pdf";
+    // PdfWriter pdfWriter = new PdfWriter(path);
+    // PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+    // pdfDocument.setDefaultPageSize(PageSize.A4);
+    // Document document = new Document(pdfDocument);
+
+    // document.add(new Paragraph("HELLO WORLD"));
+    // document.close();
+    // }
+
     @GetMapping("/public/generate-pdf/")
-    public void generatePDF() throws FileNotFoundException {
-        String path = "test.pdf";
-        PdfWriter pdfWriter = new PdfWriter(path);
+    public ResponseEntity<byte[]> generatePDF() {
+        // Tạo ByteArrayOutputStream để giữ dữ liệu PDF trong bộ nhớ
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        // Sử dụng PdfWriter để viết PDF vào ByteArrayOutputStream
+        PdfWriter pdfWriter = new PdfWriter(byteArrayOutputStream);
         PdfDocument pdfDocument = new PdfDocument(pdfWriter);
         pdfDocument.setDefaultPageSize(PageSize.A4);
         Document document = new Document(pdfDocument);
 
+        // Thêm nội dung vào tài liệu
         document.add(new Paragraph("HELLO WORLD"));
+
+        // Đóng tài liệu và PdfDocument
         document.close();
+        pdfDocument.close();
+
+        // Lấy dữ liệu PDF từ ByteArrayOutputStream
+        byte[] pdfBytes = byteArrayOutputStream.toByteArray();
+
+        // Thiết lập tiêu đề HTTP để nhắc người dùng tải xuống tệp PDF
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=test.pdf");
+        headers.add("Content-Type", "application/pdf");
+
+        // Trả lại phản hồi HTTP với dữ liệu PDF và mã trạng thái 200 (OK)
+        return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
     }
 }
